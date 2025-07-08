b867d86057d451b433ee2b79d556b5029ebfa3b7
cantcreate <adhikariprashamsa586@gmail.com>
2025-07-08 17:48:10 +0545

    created new form by using react-hook-form

--- vite-project/vite-projects/src/App.tsx
+++ vite-project/vite-projects/src/App.tsx
@@ -1,22 +1,85 @@
-import { Link,Routes, Route} from 'react-router'
-import Home from './Home'
-import About from './About'
-import Login  from './Login'
+import { useForm} from "react-hook-form"
+import './App.css'
+
 
 function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors, isSubmitting },
+  } = useForm();
   
+  async function onSubmit(data) {
+    await new Promise((resolve => setTimeout(resolve, 6000)));
+    console.log("submitting the form", data);
+  }
 
   return (
-    <>
-    <Link to="/"><p>Home</p></Link>
-    <Link to="/about"><p>About</p></Link>
-    <Link to="/Login"><p>Login</p></Link>
-      <Routes>
-        <Route path= "/" element= {<Home />} />
-        <Route path= "/about" element= {<About />} />
-        <Route path= "/login" element={<Login/>} />
-      </Routes>
-    </>
+   <form onSubmit = {handleSubmit(onSubmit)}>
+    <div>
+      <label>FirstName:</label>
+      <input {...register("firstName",
+        {
+          required: true,
+          minLength: {value:3, message:"Enter proper name"},
+          maxLength: 40
+        })}/>
+        {
+          errors.firstName &&  <p>{errors.firstName.message} </p>
+        }
+    </div>
+    <br/>
+    <div>
+      <label>MiddleName:</label>
+      <input {...register("middleName",
+        {
+          minLength: {value:3, message:"Enter proper middlename"},
+        })}/>
+        {
+          errors.middleName &&  <p>{errors.middleName.message} </p>
+        }
+    </div>
+    <br/>
+    <div>
+      <label>LastName:</label>
+      <input {...register("lastName", 
+        {
+          required: true,
+          minLength: {value:3, message:"Enter proper lastname"},
+          maxLength: 40
+        })}/>
+        {
+          errors.lastName &&  <p>{errors.lastName.message} </p>
+        }
+    </div>
+    <br/>
+    <div>
+      <label>Address:</label>
+      <input {...register("address",
+      {
+          required: true, 
+          maxLength:{value: 40, message:"Enter proper address"}
+        
+        })}/>
+        {
+          errors.address &&  <p>{errors.address.message} </p>
+        }
+    </div>
+    <br/>
+    <div>
+      <label>Phone-Number:</label>
+      <input {...register("phoneNumber")} />
+    </div>
+    <br/>
+    <div>
+      <label>Password:</label>
+      <input {...register("password")}/>
+    </div>
+    <br/>
+    <input type = "Submit"  disabled={isSubmitting}
+    value={isSubmitting ? "Submitting" : "Submit"}
+    />
+   </form> 
   )
 }
 
